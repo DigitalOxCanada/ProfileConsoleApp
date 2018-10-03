abd809e1577535dd0315e010744a33e4cbd3c0c3
Perry @DigitalOx <perry@digitalox.ca>
2018-10-03 11:51:26 -0400

    Initial Version

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,16 @@
+﻿// IntraHealth assembly - CSharp
+// #reference=System.Windows.Forms.dll
+
+using Intrahealth.Common.IHProfBL.Interop;
+using System.Windows.Forms;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(ISProfile profile, ISPatient pat, ISConstants consts)
+        {
+            MessageBox.Show("test");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ProfileConsoleApp
 C# console app for developing macro code to be run inside Profile
 
+I use this to write C# code with the richness of the class viewer and such in Visual Studio.  
+Then I copy and paste the entire page into a macro in Profile and run it from there.
